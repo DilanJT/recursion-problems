5ba68fd193aeb4ac4818220ec31d840e7da64c1c
Dilan Jayamanne <57328092+DilanJT@users.noreply.github.com>
2023-09-19 09:54:30 +0530

    implemented the logic to generate a list of the fibonaci sequence

---
+++ fibonaci/fibonaci_imp.py
@@ -0,0 +1,20 @@
+def fibonaci_sequence_implementation():
+    counter = 0
+    fibonaci_list = []
+    a, b = 0, 1
+    current_num = 0
+    fibonaci_list.append(current_num)
+    counter += 1
+    
+    current_num += 1
+    fibonaci_list.append(current_num)
+    counter += 1
+    while(counter < 8):
+        current_num = fibonaci_list[a] + fibonaci_list[b]
+        fibonaci_list.append(current_num)
+        a, b = a + 1, b + 1
+        counter += 1
+    return fibonaci_list
+    
+    
+print(fibonaci_sequence_implementation())
