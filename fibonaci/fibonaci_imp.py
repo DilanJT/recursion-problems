618dacaf1ca63c7efa1e81f935e0207d95b7ad4e
Dilan Jayamanne <57328092+DilanJT@users.noreply.github.com>
2023-09-19 10:12:31 +0530

    uupdated the fibonazi implementation

--- fibonaci/fibonaci_imp.py
+++ fibonaci/fibonaci_imp.py
@@ -1,4 +1,6 @@
-def fibonaci_sequence_implementation():
+
+
+def fibonaci_sequence_implementation(n):
     counter = 0
     fibonaci_list = []
     a, b = 0, 1
@@ -9,7 +11,7 @@ def fibonaci_sequence_implementation():
     current_num += 1
     fibonaci_list.append(current_num)
     counter += 1
-    while(counter < 8):
+    while(counter <= n):
         current_num = fibonaci_list[a] + fibonaci_list[b]
         fibonaci_list.append(current_num)
         a, b = a + 1, b + 1
@@ -17,4 +19,4 @@ def fibonaci_sequence_implementation():
     return fibonaci_list
     
     
-print(fibonaci_sequence_implementation())
+print(fibonaci_sequence_implementation(8))
